16117952e2eb124d1672f3091da9e44ca954d8bc
Sigilai5 <briansigilai@gmail.com>
2023-11-11 01:52:21 +0300

    restored images

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -85,11 +85,11 @@ const Input = () => {
         <div className="input">
             <input type="text" placeholder="Type a message" onChange={(e)=>setText(e.target.value)} value={text} />
             <div className="send">
-            {/* <img src={Attach} alt=""/>
+            <img src={Attach} alt=""/>
             <input type="file" style={{display:"none"}} id="file" onChange={(e)=>setImg(e.target.files[0])} />
             <label htmlFor="file">
                 <img src={Img} alt=""/>
-            </label> */}
+            </label>
             <button onClick={handleSend}>Send</button>
             </div>
             
