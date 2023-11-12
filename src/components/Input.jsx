ce025ce14d7d0374b2bd689bb1071b268a0384b4
Sigilai5 <briansigilai@gmail.com>
2023-11-12 12:43:53 +0300

    prevente null inputs in the keyboard

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -16,112 +16,133 @@ import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import imageCompression from "browser-image-compression";
 
 const Input = () => {
-  const [text, setText] = useState("");
-  const [img, setImg] = useState(null);
-  const [imgPreview, setImgPreview] = useState(null);
-
-  const { currentUser } = useContext(AuthContext);
-  const { data } = useContext(ChatContext);
-
-  const handleSend = async () => {
-    if (img) {
-      const options = {
-        maxSizeMB: 1,
-        maxWidthOrHeight: 500,
-        useWebWorker: true,
-      };
-
-      const compressedImage = await imageCompression(img, options);
-
-      const storageRef = ref(storage, uuid());
-      const uploadTask = uploadBytesResumable(storageRef, compressedImage);
-
-      uploadTask.on(
-        (error) => {
-          console.error("Error uploading image:", error);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-            await updateDoc(doc(db, "chats", data.chatId), {
-              messages: arrayUnion({
-                id: uuid(),
-                text,
-                date: Timestamp.now(),
-                senderId: currentUser.uid,
-                img: downloadURL,
-              }),
+    const [text, setText] = useState("");
+    const [img, setImg] = useState(null);
+    const [imgPreview, setImgPreview] = useState(null);
+    const [isButtonDisabled, setButtonDisabled] = useState(false);
+  
+    const { currentUser } = useContext(AuthContext);
+    const { data } = useContext(ChatContext);
+  
+    const handleSend = async () => {
+      if (isButtonDisabled) {
+        // If the button is disabled, do nothing
+        return;
+      }
+  
+      setButtonDisabled(true); // Disable the button
+  
+      if (!text && !img) {
+        // If both text and img are empty, do nothing
+        setButtonDisabled(false); // Re-enable the button
+        return;
+      }
+  
+      if (img) {
+        const options = {
+          maxSizeMB: 1,
+          maxWidthOrHeight: 500,
+          useWebWorker: true,
+        };
+  
+        const compressedImage = await imageCompression(img, options);
+  
+        const storageRef = ref(storage, uuid());
+        const uploadTask = uploadBytesResumable(storageRef, compressedImage);
+  
+        uploadTask.on(
+          (error) => {
+            console.error("Error uploading image:", error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+              await updateDoc(doc(db, "chats", data.chatId), {
+                messages: arrayUnion({
+                  id: uuid(),
+                  text,
+                  date: Timestamp.now(),
+                  senderId: currentUser.uid,
+                  img: downloadURL,
+                }),
+              });
             });
-          });
-        }
-      );
-    } else {
-      await updateDoc(doc(db, "chats", data.chatId), {
-        messages: arrayUnion({
-          id: uuid(),
+          }
+        );
+      } else {
+        await updateDoc(doc(db, "chats", data.chatId), {
+          messages: arrayUnion({
+            id: uuid(),
+            text,
+            date: Timestamp.now(),
+            senderId: currentUser.uid,
+          }),
+        });
+      }
+  
+      // Update last message for sender and receiver
+      await updateDoc(doc(db, "userChats", currentUser.uid), {
+        [data.chatId + ".lastMessage"]: {
           text,
-          date: Timestamp.now(),
-          senderId: currentUser.uid,
-        }),
+        },
+        [data.chatId + ".date"]: serverTimestamp(),
       });
-    }
-
-    // Update last message for sender and receiver
-    await updateDoc(doc(db, "userChats", currentUser.uid), {
-      [data.chatId + ".lastMessage"]: {
-        text,
-      },
-      [data.chatId + ".date"]: serverTimestamp(),
-    });
-    await updateDoc(doc(db, "userChats", data.user.uid), {
-      [data.chatId + ".lastMessage"]: {
-        text,
-      },
-      [data.chatId + ".date"]: serverTimestamp(),
-    });
-
-    setText("");
-    setImg(null);
-    setImgPreview(null);
-  };
-
-  const handleImageChange = (e) => {
-    const selectedImage = e.target.files[0];
-    setImg(selectedImage);
-
-    // Display image preview
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      setImgPreview(reader.result);
+      await updateDoc(doc(db, "userChats", data.user.uid), {
+        [data.chatId + ".lastMessage"]: {
+          text,
+        },
+        [data.chatId + ".date"]: serverTimestamp(),
+      });
+  
+      setText("");
+      setImg(null);
+      setImgPreview(null);
+  
+      // Re-enable the button after 1 second
+      setTimeout(() => {
+        setButtonDisabled(false);
+      }, 1000);
     };
-    reader.readAsDataURL(selectedImage);
-  };
-
-  return (
-    <div className="input">
-      <input
-        type="text"
-        placeholder="Type a message"
-        onChange={(e) => setText(e.target.value)}
-        value={text}
-      />
-      <div className="send">
+  
+    const handleImageChange = (e) => {
+      const selectedImage = e.target.files[0];
+      setImg(selectedImage);
+  
+      // Display image preview
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImgPreview(reader.result);
+      };
+      reader.readAsDataURL(selectedImage);
+    };
+  
+    return (
+      <div className="input">
         <input
-          type="file"
-          style={{ display: "none" }}
-          id="file"
-          onChange={handleImageChange}
+          type="text"
+          placeholder="Type a message"
+          onChange={(e) => setText(e.target.value)}
+          value={text}
         />
-        <label htmlFor="file">
-          {imgPreview ? (
-            <img src={imgPreview} alt="Selected" className="img-preview" />
-          ) : (
-            <img src={Img} alt="" />
-          )}
-        </label>
-        <button onClick={handleSend}>Send</button>
+        <div className="send">
+          <input
+            type="file"
+            style={{ display: "none" }}
+            id="file"
+            onChange={handleImageChange}
+          />
+          <label htmlFor="file">
+            {imgPreview ? (
+              <img src={imgPreview} alt="Selected" className="img-preview" />
+            ) : (
+              <img src={Img} alt="" />
+            )}
+          </label>
+          <button onClick={handleSend} disabled={isButtonDisabled}>
+            Send
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default Input;
+    );
+  };
+  
+  export default Input;
